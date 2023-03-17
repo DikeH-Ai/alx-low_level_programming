b6352d5db333e3f9dd96674088846da387eaf346
Dike <onyekachi.okomba@gmail.com>
2023-03-17 13:22:19 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * putchar = print char to stdout
+ * Return: 0 for success
+ */
+int main(void)
+{
+	for (char a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
