ec3ed1a9bfc0c84a0d1bafd2105954a914cd85c6
Dike <onyekachi.okomba@gmail.com>
2023-03-17 14:08:17 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	for (char a = 'a'; a <= 'z'; a++)
+	{
+		if ((a != 'e') && (a != 'q'))
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n')
+	return (0);
+}
