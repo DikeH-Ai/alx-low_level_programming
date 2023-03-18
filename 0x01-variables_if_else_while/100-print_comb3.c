bd4eac81d30736e88590e899e4191cafd6b3bf52
Dike <onyekachi.okomba@gmail.com>
2023-03-17 17:08:39 -0700

    point comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: alway return o
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if ((a != b) && (b > a))
+			{
+				putchar(48 + a);
+				putchar(48 + b);
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
