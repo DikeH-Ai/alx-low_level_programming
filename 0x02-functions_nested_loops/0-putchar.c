c59d85ee165cf5a8940e85cc8ac02189696bc135
Dike <onyekachi.okomba@gmail.com>
2023-03-21 06:13:34 -0700

    put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: return 0 if successful
+ * _putchar - prints one character at a time
+ * string - string array of characters
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
