a31a66791a637accbba1fee2a354f21244ac7143
Dike <onyekachi.okomba@gmail.com>
2023-03-21 06:43:33 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - print all lowwercae alphabets
+ * _putchar to return char values
+ * Return: this function does not rutern anything
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
