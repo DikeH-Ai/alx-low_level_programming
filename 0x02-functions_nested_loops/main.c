34b0b37cd64b6a14ed3cea15c1a5abaaf1ae245f
Dike <onyekachi.okomba@gmail.com>
2023-03-21 11:14:44 -0700

    Is_lower

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - check the code.
  *
@@ -6,6 +7,14 @@
  */
 int main(void)
 {
-    print_alphabet_x10();
+    int r;
+
+     r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
     return (0);
 }
