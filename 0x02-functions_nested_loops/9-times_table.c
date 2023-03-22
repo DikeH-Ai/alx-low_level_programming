1b439632495c21e3d358b0b712e817a8304357f5
Dike <onyekachi.okomba@gmail.com>
2023-03-21 20:50:00 -0700

    correction 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,25 +7,23 @@ void times_table(void)
 {
 	int i, j, result;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
+	for (i = 0; i <= 9; i++) {
+		for (j = 0; j <= 9; j++) {
 			result = i * j;
-			if (result > 9)
-			{
-				_putchar((result / 10) + '0');
-			} else if (i != 0)
-			{
-				_putchar(' ');
-			}
-			_putchar((result % 10) + '0');
-			if (j < 9)
-			{
+			if (j == 0) {
+				_putchar(result + '0');
+			} else if (result < 10) {
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			} else {
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
 			}
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
