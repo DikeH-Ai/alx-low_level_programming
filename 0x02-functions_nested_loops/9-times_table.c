d378b4b27a09be4965eb70b757e29694b3852134
Dike <onyekachi.okomba@gmail.com>
2023-03-21 17:44:06 -0700

    times tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+
+
+
+ */
+void times_table(void)
+{  
+	int i, j, result;
+	for (i = 0; i <= 9; i++) 
+	{
+		for (j = 0; j <= 9; j++) 
+		{
+			result = i * j;
+			if (result > 9) 
+			{
+				_putchar((result / 10) + '0');
+			} else 
+			{
+				 _putchar(' ');
+			}
+			_putchar((result % 10) + '0');
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
+}
