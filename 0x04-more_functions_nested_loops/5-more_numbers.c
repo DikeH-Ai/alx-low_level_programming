b46da9949cba8bf999db6a350b43a5c16de22653
Dike <onyekachi.okomba@gmail.com>
2023-03-23 16:49:53 -0700

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints number 1 to 14, 10 times
+ * Return: returned void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
