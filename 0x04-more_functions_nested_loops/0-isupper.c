7b01707afb7d38ebab54288eef78ba68fee75432
Dike <onyekachi.okomba@gmail.com>
2023-03-23 15:37:01 -0700

    check for uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,6 +2,9 @@
 /**
  * _isupper - Function checks is letter is uppercase or lower
  * @c: letter to be checked
+ * Description: this function takes a sigle character as an int 
+ * and returns if its upper or lowwer
+ *
  * Return: returns 1 if true else 0
  */
 int _isupper(int c)
