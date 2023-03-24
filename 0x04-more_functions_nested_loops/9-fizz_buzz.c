3a7acb73d396abe1d8fc4219d7dc8ed6e5faad24
Dike <onyekachi.okomba@gmail.com>
2023-03-23 19:55:07 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,20 +13,21 @@ int main(void)
 	{
 		if ((j % 3 == 0) && (j % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (j % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (j % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%d ",j);
+			printf("%d", j);
 		}
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
