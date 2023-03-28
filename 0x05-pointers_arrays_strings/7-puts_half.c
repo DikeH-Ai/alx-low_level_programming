d5ad746508e06459c8edd64629f0211cc40e3103
Dike <onyekachi.okomba@gmail.com>
2023-03-27 17:04:39 -0700

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half - prints half oa a string
+ * @str: string pointer
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = _strlen(str);
+
+	int j;
+
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; j <= i; j++)
+		{
+			_putchar(*(str + j));
+		}
+	}
+	else
+	{
+		for (j = ((i - 1) / 2); j <= i; j++)
+		{
+			_putchar(*(str + j));
+		}
+	}
+	_putchar('\n');
+}
+/**
+ * _strlen - count stings
+ * @s: string pointer
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (2 == 2)
+	{
+		if (*(s + i) == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	return (i);
+}
