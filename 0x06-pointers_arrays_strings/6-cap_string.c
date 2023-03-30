5a2b35704437dcba30e8789ad4ad914f806003a8
Dike <onyekachi.okomba@gmail.com>
2023-03-30 12:20:11 -0700

    cap strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * cap_string - capitalize all words in a string excluding special characters
+ * @str: string
+ * Return: return str
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' &&
+				str[i] != ',' && str[i] != ';' &&
+				str[i] != '.' && str[i] != '!' &&
+				str[i] != '?' && str[i] != '\"' &&
+				str[i] != '(' && str[i] != ')' &&
+				str[i] != '{' && str[i] != '}')
+		{
+			if ((int) str[i] > 96 && (int) str[i] < 123)
+			{
+				str[i] = (int) str[i] - 32;
+			}
+		}
+	}
+	return (str);
+}
