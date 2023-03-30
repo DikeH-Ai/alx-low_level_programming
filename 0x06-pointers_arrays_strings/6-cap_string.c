096e3cfe59952fe9115e07ddc9ca6cbaa3c47c00
Dike <onyekachi.okomba@gmail.com>
2023-03-30 16:33:00 -0700

    capo

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,21 +11,13 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ' || str[i] == '\n' ||
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
 				str[i] == ',' || str[i] == ';' ||
 				str[i] == '.' || str[i] == '!' ||
 				str[i] == '?' || str[i] == '\"' ||
 				str[i] == '(' || str[i] == ')' ||
 				str[i] == '{' || str[i] == '}')
 		{
-			if ((int) str[i + 1] > 96 && (int) str[i + 1] < 123)
-			{
-				str[i + 1] = (int) str[i + 1] - 32;
-			}
-		}
-		else if ((str[i]) == '\t')
-		{
-			str[i] = ' ';
 			if ((int) str[i + 1] > 96 && (int) str[i + 1] < 123)
 			{
 				str[i + 1] = (int) str[i + 1] - 32;
