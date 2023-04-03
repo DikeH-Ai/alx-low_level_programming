6fcd0e2dbab9e6af0d1672e59d90dcc6be26c0a3
Dike <onyekachi.okomba@gmail.com>
2023-04-03 12:06:40 -0700

    strchr function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,15 @@ char *_strchr(char *s, char c)
 	int i;
 	char *ptr;
 
-	if !(s)
-		return (NULL);
-	for (i = 0; s[i] != '\0'; i++)
+	if (s)
 	{
-		if (s[i] ==  c)
+		for (i = 0; s[i] != '\0'; i++)
 		{
-			ptr = &s[i];
-			return (ptr);
+			if (s[i] ==  c)
+			{
+				ptr = &s[i];
+				return (ptr);
+			}
 		}
 	}
 	return (NULL);
