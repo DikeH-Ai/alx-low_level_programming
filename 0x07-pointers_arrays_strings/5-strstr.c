c426e5dd84635bbb8711020b9884ddf2333bd419
Dike <onyekachi.okomba@gmail.com>
2023-04-03 22:04:05 -0700

    strstr function fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,21 +8,21 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i;
+	int j = 0;
 
-	for (i = 0; haystack[i] >= '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] >= '\0'; j++)
+		if (needle[0] == haystack[i])
 		{
-			if (haystack[i + j] != needle[j])
+			for (j = 0; needle[j] != '\0'; j++)
 			{
-				break;
-			}
-			else
-			{
-				return ((haystack + i));
+				if (haystack[i + j] != needle[j])
+					break;
 			}
 		}
+		if (needle[j] == '\0')
+			return ((haystack + i));
 	}
 	return (NULL);
 }
