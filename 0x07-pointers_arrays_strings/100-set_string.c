64cb88ee2e27e8c16f898bfbb5a808ee4b4ef656
Dike <onyekachi.okomba@gmail.com>
2023-04-04 14:47:26 -0700

    double pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *set_string - set double pointer to a pointer address
+ *@s: double pointer
+ *@to: pointer
+ *Retuen: void
+ */
+void set_string(char **s, char *to)
+{
+
+	*s = to;
+}
+
