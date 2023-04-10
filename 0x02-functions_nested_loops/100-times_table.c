01f9a4fc6b6e979d742ed32bfd3200fc6e8d4cf1
Dike <onyekachi.okomba@gmail.com>
2023-04-10 05:13:16 -0700

    merging commits

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,16 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code.
+ * print_times_table - prints time tables.
+ * @n: input
  *
- * Return: Always 0.
+ * leading_space - prints spaces.
  */
+void leading_space(int,int,int);
 
 void print_times_table(int n)
 {
-	if (n < 15 && n > 0)
+	if (n < 15 && n >= 0)
 	{
-		int i,j;
+		int i, j;
 
 		for (i = 0; i <= n; i++)
 		{
@@ -20,36 +22,45 @@ void print_times_table(int n)
 				int a = i * j;
 				int ceil = a / 10;
 				int div = a % 10;
-				if (a < 10 && a != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(a + '0');
-				}
-				else if (a < 100 && a != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(ceil + '0');
-					_putchar(div + '0');
-				}
-				else if (a < 1000 && a != 0)
-				{
-					_putchar(' ');
-					_putchar((ceil / 10) + '0');
-					_putchar((ceil % 10) + '0');
-					_putchar(div + '0');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(a + '0');
-				}
+
+				leading_space(a);
 			}
 			_putchar('\n');
 		}
 	}
+}
+void leading_space(int a, int ceil, int div)
+{
+	if (a < 10 && a != 0)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(a + '0');
+	}
+	else if (a < 100 && a != 0)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(ceil + '0');
+		_putchar(div + '0');
+	}
+	else if (a < 1000 && a != 0)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar((ceil / 10) + '0');
+		_putchar((ceil % 10) + '0');
+		_putchar(div + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(a + '0');
+				}
 }
