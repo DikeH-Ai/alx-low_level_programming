41780f60cd393c112c759b2a6df5fbc29f04bc8e
Dike <onyekachi.okomba@gmail.com>
2023-04-10 17:08:29 -0700

    nargc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: returns 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
