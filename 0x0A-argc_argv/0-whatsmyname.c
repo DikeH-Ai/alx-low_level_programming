fa0416818475fd960f1cb84c2f98acc79fdef8ec
Dike <onyekachi.okomba@gmail.com>
2023-04-10 17:01:41 -0700

    fix 0 task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,10 @@
  * main - entry point
  * @argc: argument count
  * @argv: argument vector
- * Return: returns argc
+ * Return: returns 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (argc);
+	return (0);
 }
