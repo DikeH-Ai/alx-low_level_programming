db99aad400195598a9e465c5fe11a06c0245d038
Dike <onyekachi.okomba@gmail.com>
2023-04-12 20:50:14 -0700

    concatenate strings

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+/**
+ *argstostr - concatenates all command line arguments
+ *@ac: argument count
+ *@av: argument vector
+ *Return: Returns a pointer to the new array
+ */
+char *argstostr(int ac, char **av)
+{
+	int i = 0;
+
+	int count  = 0;
+
+	int j;
+	char *ptr;
+
+	if ((ac == 0) || (av == NULL))
+		return (NULL);
+	while (i < ac)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			count++;
+		count++;
+		i++;
+	}
+	ptr = malloc(sizeof(*ptr) * count + ac);
+	i = 1;
+
+	count = 0;
+
+	if (!ptr)
+		return (NULL);
+
+
+	while (i < ac)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			ptr[count] = av[i][j];
+			count++;
+		}
+		if (ptr[count] == '\0')
+			ptr[count++] = '\n';
+		i++;
+	}
+	return (ptr);
+}
