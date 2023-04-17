c8925e2abd0a3e175ee46094cdcaadda562758eb
Dike <onyekachi.okomba@gmail.com>
2023-04-17 15:15:54 -0700

    struct syntax

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog struct
+ * @name: dogs given name
+ * @age: dogs age
+ * @owner: name of the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H*/
