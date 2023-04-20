afab2a7e301991c1fdf8646863b2006d34dacbbc
Dike <onyekachi.okomba@gmail.com>
2023-04-19 18:41:41 -0700

    int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *int_index- finds the index of an int in an array
+ *@array: array to be parsed
+ *@size: array size
+ *@cmp: function pointer
+ *Return: returns Index of sucessful else -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, x;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		x = cmp(array[i]);
+
+		if (x != 0)
+			return (i);
+	}
+	return (-1);
+}
