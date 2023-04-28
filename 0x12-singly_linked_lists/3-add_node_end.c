3124c8b3a4378810264da1ce6db175326d6fddef
Dike <onyekachi.okomba@gmail.com>
2023-04-27 21:33:59 -0700

    3 update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -33,8 +33,8 @@ list_t *add_node_end(list_t **head, const char *str)
 		while (temp->next != NULL)
 			temp = temp->next;
 		temp->next = new_node;
-	}
-	return (new_node);
+
+		return (new_node);
 	}
 	return (NULL);
 }
