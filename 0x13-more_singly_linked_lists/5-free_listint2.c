3b3c5a47432126af982fb4f1c13c0eb017946f4d
Dike <onyekachi.okomba@gmail.com>
2023-05-02 15:28:20 -0700

    free list and set head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *free_listint2 - free linked list and set head to NULL
+ *@head: linked list head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	tmp = *head;
+
+	while (*head)
+	{
+		tmp = tmp->next;
+		free(*head);
+		*head = tmp;
+	}
+	*head = NULL;
+}
