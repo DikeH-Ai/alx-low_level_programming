d32e1654b99ae375f4191e7872fc3b95b5909080
Dike <onyekachi.okomba@gmail.com>
2023-05-02 17:46:56 -0700

    number 5 fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,9 +7,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (head == NULL)
+		return;
+
 	while (*head)
 	{
-		tmp = tmp->next;
+		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
