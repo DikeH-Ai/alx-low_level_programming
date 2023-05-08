cc30635000e501da4bd40f5edcf77b9cd90a7834
Dike <onyekachi.okomba@gmail.com>
2023-05-08 15:10:55 -0700

    create and write to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,16 +23,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	while (text_content[bytes_read] != '\0')
+	if (text_content != NULL)
 	{
-		bytes_read++;
+		while (text_content[bytes_read] != '\0')
+		{
+			bytes_read++;
+		}
 	}
 
 	bytes_written = write(fd, text_content, bytes_read);
 
 	if (bytes_written == -1)
 		return (-1);
-	if(close(fd) == -1)
-		return (-1);
+	close(fd);
+
 	return (1);
 }
