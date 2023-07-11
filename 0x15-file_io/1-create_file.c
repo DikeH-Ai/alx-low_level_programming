d340a626a9e3469891e8b4d7e2aa5c613cac3efc
Dike <onyekachi.okomba@gmail.com>
2023-07-10 20:03:37 -0700

    create file fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	cnt = 1;
 
 	if (text_content != NULL)
@@ -34,7 +35,6 @@ int create_file(const char *filename, char *text_content)
 		cnt = 0;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	written_byte = write(fd, text_content, cnt);
 	if (fd == -1)
 		return (-1);
