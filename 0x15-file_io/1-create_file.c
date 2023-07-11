73caa0f13c385a10397dcb27e922b78e058f8b48
Dike <onyekachi.okomba@gmail.com>
2023-07-10 19:21:09 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,25 +18,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
-
-	if (fd == -1)
-	{
-		if (errno == EEXIST)
-		{
-			fd = open(filename, O_TRUNC);
-
-			if (fd == -1)
-			{
-				return (-1);
-			}
-		}
-		else
-		{
-			return (-1);
-		}
-	}
-	cnt = 0;
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	cnt = 1;
 
 	while (text_content[cnt] != '\0')
 	{
