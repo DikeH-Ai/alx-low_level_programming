093e9bfbc6419d411f3d35ab7f5a4f929c48f201
Dike <onyekachi.okomba@gmail.com>
2023-07-10 19:04:27 -0700

    Create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,54 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <errno.h>
+/**
+ * create_file - creates file, trncate file if it already exsit
+ * @filename: name fof the file
+ * @text_content: content to be written to the file
+ * Return: 1 or -1 for succes or failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, cnt, written_byte;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
+
+	if (fd == -1)
+	{
+		if (errno == EEXIST)
+		{
+			fd = open(filename, O_TRUNC);
+
+			if (fd == -1)
+			{
+				return (-1);
+			}
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	cnt = 0;
+
+	while (text_content[cnt] != '\0')
+	{
+		written_byte = write(fd, &text_content[cnt], 1);
+
+		if (written_byte < 0)
+			return (-1);
+
+		cnt++;
+	}
+	write(fd, "", 1);
+
+	close(fd);
+	return (1);
+}
