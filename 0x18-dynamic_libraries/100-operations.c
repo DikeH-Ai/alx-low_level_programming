448d5ce2baa813f9b8a52bd26ecff7615d86d1be
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-26 04:27:15 -0700

    shared lib to py

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ *add - adds int
+ *@a: int a
+ *@b: int b
+ *Return: value of a + b
+ */
+int add(int a, int b)
+{       
+	return (a + b);
+}       
+
+/**
+ *sub - subs int
+ *@a: int a
+ *@b: int b
+ *Return: value of a - b
+ */
+int sub(int a, int b)
+{       
+	return (a - b);
+}   
+
+/**
+ *mul - multiply ints
+ *@a: int a
+ *@b: int b
+ *Return: value of a * b
+ */
+int mul(int a, int b)
+{       
+	return (a * b);
+}
+
+/**
+ *div - divides int
+ *@a: int a
+ *@b: int b
+ *Return: value of a / b
+ */
+int div(int a, int b)
+{       
+	return (a / b);
+}   
+
+/**
+ *mod - find modulus
+ *@a: int a
+ *@b: int b
+ *Return: value of a % b
+ */
+int mod(int a, int b)
+{       
+	return (a % b);
+} 
---
+++ 0x18-dynamic_libraries/mathops.h
@@ -0,0 +1,8 @@
+#ifndef MATHOPS_H
+#define MATHOPS_H
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+#endif /*mathops_h*/
