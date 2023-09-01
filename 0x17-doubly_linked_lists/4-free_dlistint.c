0726b60730e73f4ff976ff3302cc6ce393834e0c
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-09-01 15:53:04 -0700

    free double linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_dlistint - free list recursively
+ * @head: dlist head
+ * Return: Non
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	temp = head;
+
+	while (head->next != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	free(temp);
+}
